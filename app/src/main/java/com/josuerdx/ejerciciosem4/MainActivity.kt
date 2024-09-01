bb1dd0e68298352da303d134fa670caa67dc5352
JosueRDx <rodrigo.apaza.m@tecsup.edu.pe>
2024-09-01 23:05:29 +0200

    chore: setup MainActivity and MainScreen

--- app/src/main/java/com/josuerdx/ejerciciosem4/MainActivity.kt
+++ app/src/main/java/com/josuerdx/ejerciciosem4/MainActivity.kt
@@ -4,13 +4,18 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.josuerdx.ejerciciosem4.ui.theme.EjercicioSem4Theme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +24,25 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             EjercicioSem4Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                MainScreen()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
+fun MainScreen(modifier: Modifier = Modifier) {
+    Column(
         modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    EjercicioSem4Theme {
-        Greeting("Android")
+            .fillMaxSize()
+            .padding(30.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Top
+    ) {
+        Text(
+            text = "Estado del Guante",
+            style = MaterialTheme.typography.headlineMedium,
+            modifier = Modifier.padding(bottom = 10.dp)
+        )
     }
 }
